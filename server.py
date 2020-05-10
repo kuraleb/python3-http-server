96378ab01a150bd060232a99a0f9cd8f8b131238
kuraleb <35849438+kuraleb@users.noreply.github.com>
2020-05-10 22:32:31 +0300

    Change port

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 HOST_NAME = 'httpserver-test.127.0.0.1.nip.io'
-PORT_NUMBER = 8080
+PORT_NUMBER = 80
 
 
 class MyHandler(BaseHTTPRequestHandler):
